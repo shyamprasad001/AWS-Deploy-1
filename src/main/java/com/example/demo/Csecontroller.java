10f68be9e593d3e2775224387bbd8f2762f625de
Shyam Mantri <iamshyam1911@gmail.com>
2026-02-25 06:45:20 +0530

    first commit

---
+++ src/main/java/com/example/demo/Csecontroller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Csecontroller {
+	
+	@GetMapping("/home")
+	public String rocks() {
+		return "Welcome to AWS Jenkins";
+	}
+	
+	@GetMapping("/cse")
+	public String rocks1() {
+		return "Hello all";
+	}
+	
+}
